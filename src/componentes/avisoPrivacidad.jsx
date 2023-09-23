815428c3996fc639d7a963edd300d3292a0ea194
isaiale <isaialef20@gmail.com>
2023-09-23 17:30:52 -0600

    d

--- src/componentes/avisoPrivacidad.jsx
+++ src/componentes/avisoPrivacidad.jsx
@@ -1,55 +1,159 @@
 import React from "react";
 
 function AvisoPrivacidad() {
-    return (
-      <div className="App">
-        <div>
-          <div className="text-center"><h2>AVISO DE PRIVACIDAD</h2></div>
-          <div >
-            <h3 className="text-center">¿Quiénes somos?</h3>
-            <p className="ms-5 me-5 ">
-              Dalilaa Del Angel, mejor conocido como Mi Enfermera Favorita, con domicilio en
-              calle C. San Luis Potosi #30, colonia Tahuizan, ciudad Huejutla de Reyes,
-              municipio o delegación Huejutla de Reyes, c.p. 43000, en la entidad de Hidalgo,
-              país Mexico, y portal de internet
-              https://web.facebook.com/profile.php?id=100084107476329&sk=about, es el
-              responsable del uso y protección de sus datos personales, y al respecto le
-              informamos lo siguiente:
-            </p>
-            <h3 className="text-center ">¿Para qué fines utilizaremos sus datos personales?</h3>
-            <p className="ms-5 me-5 ">De manera adicional, utilizaremos su información personal para las siguientes
-              finalidades secundarias que no son necesarias para el servicio solicitado, pero
-              que nos permiten y facilitan brindarle una mejor atención:</p>
-              <p className="ms-5 me-5">●  Marketing y Comunicaciones</p>
-              <p className="ms-5 me-5">● Análisis y Mejora del Sitio Web</p>
-              <p className="ms-5 me-5">● Procesamiento de Pedidos</p>
-              <p className="ms-5 me-5">● Mercadotecnia o publicitaria</p>
-              <p className="ms-5 me-5">● Prospección comercial</p>
-              <p className="ms-5 me-5">En caso de que no desee que sus datos personales se utilicen para estos fines
-              secundarios, indíquelo a continuación:</p>
-              <p className="ms-5 me-5">No consiento que mis datos personales se utilicen para los siguientes fines:</p>
-              <p className="ms-5 me-5">[ ] Marketing y Comunicaciones</p>
-              <p className="ms-5 me-5">[ ] Análisis y Mejora del Sitio Web</p>
-              <p className="ms-5 me-5">[ ] Procesamiento de Pedidos</p>
-              <p className="ms-5 me-5">[ ] Mercadotecnia o publicitaria</p>
-              <p className="ms-5 me-5">[ ] Prospección comercial</p>
-              
-              <p className="ms-5 me-5">La negativa para el uso de sus datos personales para estas finalidades no podrá
-              ser un motivo para que le neguemos los servicios y productos que solicita o
-              contrata con nosotros.</p>
-            
-            <h3 className="text-center">¿Dónde puedo consultar el aviso de privacidad integral?</h3>
-            <p className="ms-5 me-5">Para conocer mayor información sobre los términos y condiciones en que serán
-              tratados sus datos personales, como los terceros con quienes compartimos su
-              información personal y la forma en que podrá ejercer sus derechos ARCO, puede
-              consultar el aviso de privacidad integral en:
-              Enlace en el Sitio Web, Durante el Registro o Compra y Enlaces en
-              ComunicacionesF</p>
-          </div>
+  return (
+    <div className="App">
+      <div>
+        <div className="text-center"><h2>AVISO DE PRIVACIDAD</h2></div>
+        <div  className="ms-5 me-5 fs-5">
+
+          <h3 className="text-center">Empresa: Mi Enfermera Favorita</h3>
+          <p className="ms-5 me-5 ">
+            Domicilio: Dalilaa Del Angel, mejor conocido como Mi Enfermera Favorita, con
+            domicilio en calle C. San Luis Potosi #30, colonia Tahuizan, ciudad Huejutla de Reyes,
+            municipio o delegación Huejutla de Reyes, c.p. 43000, en la entidad de Hidalgo, país
+            Mexico, y portal de internet
+            https://web.facebook.com/profile.php?id=100084107476329&sk=about, es el
+            responsable del uso y protección de sus datos personales, y al respecto le
+            informamos lo siguiente:
+          </p>
+          <h3 className="text-center ">Responsabilidad en la Recopilación de Datos Personales:</h3>
+          <p className="ms-5 me-5 ">
+            En "Mi Enfermera Bonita", reconocemos nuestra responsabilidad en la recopilación
+            de sus datos personales. Nos comprometemos a recopilar únicamente la
+            información necesaria y relevante para los fines establecidos en nuestro aviso de
+            privacidad. No recopilaremos datos personales sin su consentimiento expreso y
+            utilizaremos medios seguros para su obtención.
+
+          </p>
+          <p className="ms-5 me-5">
+            <span className="text-dark fs-4"> Registro y Gestión de Cuentas: </span>
+            Utilizaremos sus datos personales, como su
+            nombre, dirección de correo electrónico y contraseña, para crear y administrar
+            su cuenta en nuestro sitio web. Esto le permitirá realizar compras en línea,
+            programar servicios de enfermería y acceder a información personalizada.
+          </p>
+          <p className="ms-5 me-5">
+            <span className="text-dark fs-4">  Procesamiento de Compras: </span>
+            Si decide realizar una compra a través de
+            nuestro sitio web, utilizaremos sus datos personales para procesar la
+            transacción, incluida la facturación y el envío de productos
+          </p>
+          <p className="ms-5 me-5">
+            <span className="text-dark fs-4">  Programación de Servicios: </span>
+            Si opta por utilizar nuestros servicios de alquiler
+            de ropa de enfermería, utilizaremos sus datos personales para programar y
+            coordinar la entrega y devolución de los productos de manera eficiente.
+          </p>
+          <p className="ms-5 me-5">
+            <span className="text-dark fs-4"> Comunicación y Soporte al Cliente: </span>
+            Podemos utilizar su información de
+            contacto, como su dirección de correo electrónico y número de teléfono, para
+            comunicarnos con usted sobre su cuenta, pedidos, servicios programados y
+            para brindar asistencia al cliente cuando sea necesario.
+          </p>
+          <p className="ms-5 me-5">
+            <span className="text-dark fs-4">  Mejora de Servicios: </span>
+            : Utilizaremos datos anónimos y agregados para analizar
+            el comportamiento de los usuarios en nuestro sitio web y mejorar nuestros
+            productos y servicios en función de las preferencias y necesidades de nuestros
+            clientes.
+          </p>
+
+
+          <h3 className="ms-5 me-5">ARCO:</h3>
+          <p className="ms-5 me-5">En "Mi Enfermera Bonita", respetamos tu privacidad y te ofrecemos los siguientes
+            derechos sobre tus datos personales:
+          </p>
+
+          <p className="ms-5 me-5">● Acceso: Puedes solicitar acceder a tus datos personales en cualquier
+            momento.</p>
+
+          <p className="ms-5 me-5">● Rectificación: Si encuentras errores en tus datos, puedes pedir que los
+            corrijamos.</p>
+          <p className="ms-5 me-5">● Cancelación: Si deseas que eliminemos tus datos, podemos hacerlo según lo
+            permitido por la ley.</p>
+          <p className="ms-5 me-5">● Oposición: Tienes derecho a oponerte al uso de tus datos para fines
+            específicos.</p>
+
+          <p className="ms-5 me-5">Para ejercer estos derechos, [contacta con nosotros] o [utiliza nuestro formulario en
+            línea]. Tu privacidad es importante para nosotros, y estamos aquí para ayudarte.</p>
+          <h3 className="ms-5 me-5">Datos de contacto:</h3>
+
+          <p className="ms-5 me-5">Gmail: mienfermerafavorita@gmail.com</p>
+          <p className="ms-5 me-5">Tel: 771 722 8532</p>
+          <p className="ms-5 me-5">Domicilio: C. San Luis Potosi #30, colonia Tahuizan, ciudad Huejutla de Reyes</p>
+
+          <h3 className="ms-5 me-5">Transferencias de información:</h3>
+          <p className="ms-5 me-5">● Compartiremos sus datos personales con el propósito de procesar sus
+            pedidos y proporcionar nuestros servicios de alquiler de ropa de enfermería
+            y venta de productos de enfermería.</p>
+          <p className="ms-5 me-5"> ● Los datos personales pueden ser compartidos con empresas de envío y
+            logística para entregar sus pedidos de manera eficiente.
+          </p>
+          <p className="ms-5 me-5">● Podemos compartir información con terceros proveedores de servicios que
+            nos ayuden en funciones comerciales como el procesamiento de pagos y el
+            análisis de datos.
+          </p>
+
+          <p className="ms-5 me-5">● En algunos casos, podemos compartir datos para cumplir con obligaciones
+            legales y regulaciones aplicables.
+          </p>
+
+          <h3 className="text-center">No autorización de transferencia:</h3>
+          <p className="ms-5 me-5">En "Mi Enfermera Bonita", respetamos su derecho a tomar decisiones sobre el uso y
+            la transferencia de sus datos personales. Si en algún momento decide no autorizar
+            la transferencia de sus datos personales en los términos que señala este aviso de
+            privacidad, por favor tenga en cuenta lo siguiente:</p>
+
+          <p className="ms-5 me-5">● La no autorización de la transferencia de sus datos personales podría afectar
+            nuestra capacidad para brindarle algunos de nuestros servicios, como el
+            procesamiento de pedidos y la entrega de productos.</p>
+          <p className="ms-5 me-5">● Es posible que no podamos cumplir con ciertas obligaciones legales o
+            regulaciones que requieran la transferencia de datos personales.</p>
+          <p className="ms-5 me-5">● Al no autorizar la transferencia de datos, es posible que no pueda aprovechar
+            completamente todas las características y servicios que ofrecemos.</p>
+
+          <p className="ms-5 me-5">Respetamos su decisión y su privacidad es importante para nosotros. Si decide no
+            autorizar la transferencia de sus datos personales o tiene alguna pregunta
+            relacionada con este tema, [por favor póngase en contacto con nuestro equipo de
+            privacidad]. Estamos aquí para brindarle la información y la asistencia que pueda
+            necesitar.<br /><br />
+            Tenga en cuenta que, al continuar utilizando nuestros servicios y no manifestar una
+            oposición explícita a la transferencia de datos, se entenderá que ha dado su
+            consentimiento para que sus datos personales se utilicen y se transfieran de acuerdo
+            con los términos establecidos en este aviso de privacidad. </p>
+
+
+
+          <h3 className="text-center">Para Dejar de Recibir Mensajes Promocionales:</h3>
+          <p className="ms-5 me-5">En "Mi Enfermera Bonita", respetamos su privacidad y entendemos que sus
+            preferencias pueden cambiar con el tiempo. Si en algún momento desea dejar de
+            recibir mensajes promocionales de nuestra parte, puede hacerlo de las siguientes
+            maneras:<br /><br />
+            <span className="text-dark fs-5">1. Opción de Cancelación de Suscripción:</span>
+
+             Cada mensaje promocional que reciba
+            de nosotros contendrá un enlace o una opción para cancelar su suscripción.
+            Haga clic en esta opción para dejar de recibir mensajes promocionales en el
+            futuro.<br /><br />
+            <span className="text-dark fs-5">2. Contacto Directo:</span>
+
+             También puede ponerse en contacto con nosotros a través
+            de 771 722 8532 o mienfermerafavorita@gmail.com. Indique su deseo de
+            dejar de recibir mensajes promocionales, y procesaremos su solicitud de
+            manera oportuna.<br /><br />
+            Tenga en cuenta que, incluso si opta por no recibir mensajes promocionales, es
+            posible que aún reciba comunicaciones relacionadas con transacciones, como
+            confirmaciones de pedidos y actualizaciones de cuenta.
+          </p>
+          <h3 className="ms-5 me-5">Última actualización: <span className="text-dark fs-5"> 22/09/2023</span></h3>
+
+
         </div>
       </div>
-    );
-  }
-  
-  export default AvisoPrivacidad;
-  
+    </div>
+  );
+}
+
+export default AvisoPrivacidad;
