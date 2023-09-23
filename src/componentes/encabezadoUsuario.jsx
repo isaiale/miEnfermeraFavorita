ad8f9469b86e92c7d898bff213478bd57788fb25
isaiale <isaialef20@gmail.com>
2023-09-22 22:34:51 -0600

    k

--- src/componentes/encabezadoUsuario.jsx
+++ src/componentes/encabezadoUsuario.jsx
@@ -10,7 +10,7 @@ const EncabezadoUser = () => {
             <div className="bg-primary-200 text-text-100 p-3 fadeInColor" style={{ minHeight: '550px' }}>
                 <div className="d-flex flex-column-reverse flex-lg-row align-items-lg-center">
                     <div className="w-100">
-                        <div className="mt-5">
+                        <div className="mt-1">
                             <p className="text-center custom-text">¡Descubre nuestros productos que tenemos a la venta!</p>
                             <div className="text-center mt-3">
                                 <Link className="btn fs-4" style={{background: '#0a7fad', color:'white'}} to="/avisoPrivacidad">Aviso de Privacidad</Link>
@@ -18,7 +18,7 @@ const EncabezadoUser = () => {
                         </div>
                     </div>
                     <div className="w-100 text-center">
-                        <img src={drImg} alt="Imagen de la empresa" className="img-fluid mt-5" />
+                        <img src={drImg} alt="Imagen de la empresa" className="img-fluid mt-1 w-50" />
                     </div>
                 </div>
             </div>
