132a02ee4216818c26e604ada8ffacc6bac08fd3
isaiale <isaialef20@gmail.com>
2023-10-11 23:11:26 -0600

    fgsrth

--- src/componentes/encriptacion.jsx
+++ src/componentes/encriptacion.jsx
@@ -1,75 +1,81 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
 
-class Encriptacion extends Component {
-  constructor() {
-    super();
-    this.state = {
-      plaintext: '',
-      shift: 0,
-      ciphertext: '',
-    };
-  }
+function CaesarCipher() {
+  const [text, setText] = useState('');
+  const [shift, setShift] = useState(4);
+  const [result, setResult] = useState('');
+  const [decryptedText, setDecryptedText] = useState('');
 
-  encryptCesar = () => {
-    const { plaintext, shift } = this.state;
-    const shiftedText = plaintext
+  const encrypt = () => {
+    const encryptedText = text
       .split('')
-      .map((char) => {
-        if (char.match(/[a-z]/i)) {
-          const charCode = char.charCodeAt(0);
-          let shiftAmount = shift % 26;
-          if (char.match(/[a-z]/ && charCode + shiftAmount > 'z'.charCodeAt(0))) {
-            shiftAmount -= 26;
-          } else if (char.match(/[A-Z]/ && charCode + shiftAmount > 'Z'.charCodeAt(0))) {
-            shiftAmount -= 26;
-          }
-          return String.fromCharCode(charCode + shiftAmount);
+      .map(char => {
+        if (char.match(/[a-zA-ZñÑ]/)) {
+          const isUpperCase = char === char.toUpperCase();
+          const alphabet = isUpperCase ? 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ' : 'abcdefghijklmnñopqrstuvwxyz';
+          const charIndex = alphabet.indexOf(char);
+          const shiftedIndex = (charIndex + shift) % 28; // Ajustar al tamaño del alfabeto con la 'ñ'
+          return isUpperCase ? alphabet[shiftedIndex] : alphabet[shiftedIndex].toLowerCase();
         }
         return char;
       })
       .join('');
-    this.setState({ ciphertext: shiftedText });
+    setResult(encryptedText);
   };
 
-  handlePlaintextChange = (event) => {
-    this.setState({ plaintext: event.target.value });
+  const decrypt = () => {
+    const decryptedText = result
+      .split('')
+      .map(char => {
+        if (char.match(/[a-zA-ZñÑ]/)) {
+          const isUpperCase = char === char.toUpperCase();
+          const alphabet = isUpperCase ? 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ' : 'abcdefghijklmnñopqrstuvwxyz';
+          const charIndex = alphabet.indexOf(char);
+          const shiftedIndex = (charIndex - shift + 28) % 28; // Ajustar al tamaño del alfabeto con la 'ñ'
+          return isUpperCase ? alphabet[shiftedIndex] : alphabet[shiftedIndex].toLowerCase();
+        }
+        return char;
+      })
+      .join('');
+    setDecryptedText(decryptedText);
   };
 
-  handleShiftChange = (event) => {
-    this.setState({ shift: parseInt(event.target.value, 10) });
-  };
-
-  render() {
-    return (
-      <div className="container mt-5">
-        <h2>Cifrado César</h2>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Texto a cifrar"
-            value={this.state.plaintext}
-            onChange={this.handlePlaintextChange}
-          />
-        </div>
-        <div className="mb-3">
-        <h3>Desplazamiento</h3>
-          <input
-            type="number"
-            className="form-control"
-            placeholder="Desplazamiento"
-            value={this.state.shift}
-            onChange={this.handleShiftChange}
-          />
-        </div>
-        <button className="btn btn-success" onClick={this.encryptCesar}>
-          Cifrar Texto
-        </button>
-        <p className="mt-3">Cifrado: {this.state.ciphertext}</p>
+  return (
+    <div className="container border mt-5">
+      <h1 className="mt-4 text-center">Cifrado César</h1>
+      <div className="form-group">
+        <label htmlFor="text" className='fs-4'>Texto a cifrar:</label>
+        <input
+          type="text"
+          id="text"
+          className="form-control"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
-    );
-  }
+      <div className="form-group">
+        <label htmlFor="shift" className='fs-4'>Desplazamiento:</label>
+        <input
+          type="number"
+          id="shift"
+          className="form-control"
+          value={shift}
+          onChange={(e) => setShift(parseInt(e.target.value, 10))}
+        />
+      </div>
+      <button className="btn btn-primary m-2" onClick={encrypt}>Cifrar</button>
+      <button className="btn btn-success m-2" onClick={decrypt}>Desencriptar</button>
+
+      <div className="mt-4">
+        <h3>Texto cifrado:</h3>
+        <p>{result}</p>
+      </div>
+      <div className="mt-4">
+        <h3>Texto desencriptado:</h3>
+        <p>{decryptedText}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Encriptacion;
+export default CaesarCipher;
