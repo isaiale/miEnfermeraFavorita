38f31ccd39b455ae29da301e7c95da249ea699ae
isaiale <isaialef20@gmail.com>
2024-04-10 00:19:36 -0600

    corregido

--- src/errores/canceled.js
+++ src/errores/canceled.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import NavbarUsuario from '../componentes/navbarUsuario';
+import Footer from '../componentes/footer';
+import { Link } from 'react-router-dom';
+// import '../css/carritoCompras.css'
 
 const Cancelado = () => {
   return (
     <div>
-      <div className='mt-5 mb-5' style={{ backgroundColor: 'lightcoral', padding: '10px', borderRadius: '5px', textAlign: 'center' }}>
-        <h2>¡Cancelado!</h2>
-        <p>¡Operación cancelada!</p>
+      <NavbarUsuario />
+      <div className='m-5'>
+        <div className='mt-5 mb-5' style={{ backgroundColor: 'lightcoral', padding: '10px', borderRadius: '5px', textAlign: 'center' }}>
+          <h2>¡Cancelado!</h2>
+          <p>¡Operación cancelada!</p>
+          <Link className='fs-5 text-dark' to='/'>Ir a Inicio</Link>
+        </div>
       </div>
+
+      <Footer />
     </div>
   )
 }
