72018da1eafa029e9ab55371f51ed39b2cb064d2
isaiale <isaiale20@gmail.comm>
2024-07-23 03:22:49 -0600

    key

--- src/componentes/login.js
+++ src/componentes/login.js
@@ -10,7 +10,7 @@ import '../css/colores.css';
 import Swal from "sweetalert2";
 import Breadcrumb from '../utilidad/migapan';
 
-const vapidPublicKey = 'BL18qSRqj2Na9VxYd8_H7mXQ7MnrdUtB3ZXZ-3vTKMONi3qZZnDenBIU6nLOczFDr-EU5U7GwMHo0jIH-liA7Zk';
+const vapidPublicKey = 'BPuoxpP6by2PsT6vp3o_ohnBE7fqeQq9O5-xZ1WWwbs34UbDTDJaI_hAe1CFq9CdWViG764lIiRpK68xHiuO15c';
 
 function Login() {
     const [email, setEmail] = useState('');
