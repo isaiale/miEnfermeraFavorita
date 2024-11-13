526c55af34caf2b1a30a3ee322c20ccba415accd
isaiale <isaialef20@gmail.com>
2024-11-13 00:43:52 -0600

    ajuste de index

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import { AuthContextProvider } from './autenticar/AuthProvider';
-import { askPermission, subscribeUserToPush } from './utilidad/pushNotifications';
-import SplashScreen from './SplashScreen';
-
+import { askPermission, subscribeUserToPush } from './utilidad/pushNotifications'; // Importa las funciones de notificaciones push
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.jsx')
+    navigator.serviceWorker.register('/service-worker.js')
       .then((registration) => {
         console.log('Service Worker registrado con éxito:', registration);
-
         // Pedir permiso para las notificaciones push
         askPermission().then((permission) => {
           if (permission === 'granted') {
@@ -29,28 +26,68 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
-const Root = () => {
-  const [showSplash, setShowSplash] = useState(true);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>
+);
 
-  useEffect(() => {
-    // Ocultar el splash después de 1 segundo
-    const timer = setTimeout(() => {
-      setShowSplash(false);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
 
-  return (
-    <React.StrictMode>
-      {showSplash ? (
-        <SplashScreen /> // Mostrar SplashScreen
-      ) : (
-        <AuthContextProvider>
-          <App />
-        </AuthContextProvider>
-      )}
-    </React.StrictMode>
-  );
-};
+// import React, { useState, useEffect } from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/js/bootstrap.bundle';
+// import { AuthContextProvider } from './autenticar/AuthProvider';
+// import { askPermission, subscribeUserToPush } from './utilidad/pushNotifications';
+// import SplashScreen from './SplashScreen';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.jsx')
+//       .then((registration) => {
+//         console.log('Service Worker registrado con éxito:', registration);
+
+//         // Pedir permiso para las notificaciones push
+//         askPermission().then((permission) => {
+//           if (permission === 'granted') {
+//             // Suscribir al usuario a las notificaciones push
+//             subscribeUserToPush(registration);
+//           } else {
+//             console.log('Permiso de notificación no concedido');
+//           }
+//         });
+//       })
+//       .catch((error) => {
+//         console.log('Error al registrar el Service Worker:', error);
+//       });
+//   });
+// }
+// const Root = () => {
+//   const [showSplash, setShowSplash] = useState(true);
+
+//   useEffect(() => {
+//     // Ocultar el splash después de 1 segundo
+//     const timer = setTimeout(() => {
+//       setShowSplash(false);
+//     }, 1000);
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <React.StrictMode>
+//       {showSplash ? (
+//         <SplashScreen /> // Mostrar SplashScreen
+//       ) : (
+//         <AuthContextProvider>
+//           <App />
+//         </AuthContextProvider>
+//       )}
+//     </React.StrictMode>
+//   );
+// };
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
