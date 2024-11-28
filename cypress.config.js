3b4ebb873778e1fa2a7c80db3281d7516214b7d7
JamesB15Code <jamesbrian1504@gmail.com>
2024-11-28 02:05:32 -0600

    Configuracion ci-cd pruebas cypress

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:5173', // Ajusta el puerto si es necesario
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', 
+    supportFile: false, // Desactiva el archivo de soporte si no necesitas configuraciones avanzadas
+  },
+});
