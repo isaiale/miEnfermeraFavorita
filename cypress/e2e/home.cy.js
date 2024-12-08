311eac16436199a555e4917b0d5d084cbed11d29
JamesB15Code <jamesbrian1504@gmail.com>
2024-12-08 02:02:19 -0600

    pruebas cypress

--- cypress/e2e/home.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Home Page Tests', () => {
-  it('Debería cargar correctamente la página principal', () => {
-    cy.visit('/'); // Navega al Home de tu aplicación
-    cy.get('h1').contains('Bienvenido'); // Verifica que el encabezado contiene "Bienvenido"
-  });
-
-});
